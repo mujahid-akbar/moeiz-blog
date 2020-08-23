3ff3ec31680984fa0f5edb9b93fe891ffb9302b3
mujahid-akbar <51745086+mujahid-akbar@users.noreply.github.com>
2020-08-23 09:01:21 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Moeiz's Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Moeiz Ahmed`,
+      summary: ``,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    description: `A blog for my thoughts sharing`,
+    siteUrl: `https://moeiz.netlify.app/`,
   },
   plugins: [
     `gatsby-plugin-netlify-cms`,
